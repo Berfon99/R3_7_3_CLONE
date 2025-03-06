3ec28e6e87f8a2b57a7f3df1e4036d732e81f8c3
Berfon99 <bertrand@lesfontaines.be>
2025-03-06 12:32:03 +0100

    first step

---
+++ app/src/main/java/com/xc/r3/ModelSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.xc.r3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModelSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model_selection);
+    }
+}
